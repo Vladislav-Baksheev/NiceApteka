84c04398b65b66fc0ac897b8d6e9bbaf42d5fead
Владислав Бакшеев <bogomolleha7@gmail.com>
2025-02-04 01:45:50 +0700

    Добавлен OrderController

---
+++ NiceApteka/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NiceApteka.Data;
+using NiceApteka.Models;
+
+namespace NiceApteka.Controllers
+{
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly NiceaptekaContext _db;
+        public OrderController(NiceaptekaContext db)
+        {
+            _db = db;
+        }
+
+        [Route("orders")]
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            var orders = _db.Orders;
+            if (orders == null)
+            {
+                return NotFound();
+            }
+            return Ok(orders);
+        }
+
+        [Route("order/{id}")]
+        [HttpGet]
+        public IActionResult GetOrderById([FromRoute] int id)
+        {
+            var order = _db.Orders.FirstOrDefault(p => p.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
+
+        [Route("order/add")]
+        [HttpPost]
+        public IActionResult AddOrder(Order order)
+        {
+            try
+            {
+                _db.Orders.Add(order);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            if (order == null)
+            {
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(GetOrderById), new { id = order.OrderId }, order);
+        }
+    }
+}
