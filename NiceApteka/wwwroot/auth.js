40ba38308671073ba06134da63cd9a36f3ffc9a4
Владислав Бакшеев <bogomolleha7@gmail.com>
2025-02-19 21:22:21 +0700

    Реализован вход, создание куки, отображение товаров

---
+++ NiceApteka/wwwroot/auth.js
@@ -0,0 +1,109 @@
+﻿const authCookieName = 'auth_cookie';
+let user;
+function init() {
+    let cookieName = getCookie(authCookieName);
+    if (cookieName != null) {
+        user = {
+            name: cookieName
+        };
+        document.getElementById('enter').classList.remove('hidden');
+        document.getElementById('enter').innerText = user.name;
+        window.location.href ='index.html'
+    }
+}
+
+function switchToRegister() {
+    let divReg = document.querySelector('.regBlock');
+    let divAuth = document.querySelector('.authBlock');
+
+    divAuth.style.display = "none";
+    divReg.style.display = "flex";
+}
+
+function switchToLogin() {
+    let divReg = document.querySelector('.regBlock');
+    let divAuth = document.querySelector('.authBlock');
+
+    divAuth.style.display = "flex"; 
+    divReg.style.display = "none";
+}
+
+function login() {
+    const addEmailTextbox = document.getElementById('loginEmail'); 
+    const addPassTextbox = document.getElementById('loginPass'); 
+
+    const user = {
+        Email: addEmailTextbox.value.trim(),
+        PasswordHash: addPassTextbox.value.trim()
+    }
+
+    fetch("auth/user", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => response.json())
+        .then(setCookie(authCookieName, user.Email, 1))
+        .then(window.location.href = 'auth.html')
+        .catch(error => console.error('Unable to login.', error));
+}
+
+function register() {
+    const registerEmailTextbox = document.getElementById('registerEmail');
+    const registerPassTextbox = document.getElementById('registerPassword');
+    const repeatRegisterPassTextbox = document.getElementById('repeatRegisterPassword');
+
+    if (registerPassTextbox.value.trim() != repeatRegisterPassTextbox.value.trim()) {
+        alert("Пароли должны совпадать!");
+    }
+    else {
+        const user = {
+            Email: registerEmailTextbox.value.trim(),
+            PasswordHash: registerPassTextbox.value.trim()
+        }
+
+        fetch("register/user", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => response.json())
+            .catch(error => console.error('Unable to register.', error));
+    }
+}
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function deleteCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+}
---
+++ NiceApteka/wwwroot/scripts.js
@@ -0,0 +1,92 @@
+﻿let products = [];
+const authCookieName = 'auth_cookie';
+let user; 
+
+function init(){
+    getProducts();
+    let cookieName = getCookie(authCookieName);
+    if (cookieName != null) {
+        user = {
+            name: cookieName
+        };
+        document.getElementById('exitBtn').classList.remove('hidden');
+        document.getElementById('enter').innerText = user.name;
+    }
+    else {
+        document.getElementById('exitBtn').classList.add('hidden');
+    }
+}
+
+function getProducts() {
+    fetch("products")
+        .then(reponse => reponse.json())
+        .then(data => _displayProducts(data))
+        .catch(error => console.error('Unable to get products.', error));
+}
+
+function _displayProducts(data) {
+    data.forEach(product => {
+        //div's
+        var divCard = document.createElement("div");
+        var divPhoto = document.createElement("div");
+        var divDescription = document.createElement("div");
+
+        //    <h2>Название</h2>
+        //    <h4>Категория</h4>
+        //    <h1>Цена</h1>
+        //    <p>Описание</p>
+        //    <button>Добавить в корзину</button>
+
+        //elements in div's
+        var img = document.createElement("img");
+
+        var productName = document.createElement("h2");
+        var productCategory = document.createElement("h4");
+        var productPrice = document.createElement("h1");
+        var productDescription = document.createElement("p");
+        var addToCartBtn = document.createElement("button");
+
+
+        divCard.className = "card";
+        divPhoto.className = "photo";
+        divDescription.className = "description";
+
+        img.src = product.imageUrl;
+
+        productName.textContent = product.name;
+        /*productCategory.textContent = ;*/
+        productPrice.textContent = product.price;
+        productDescription.textContent = product.description;
+        addToCartBtn.textContent = "Добавить в корзину";
+
+        divPhoto.appendChild(img);
+        divDescription.appendChild(productName);
+        divDescription.appendChild(productPrice);
+        divDescription.appendChild(productDescription);
+        divDescription.appendChild(addToCartBtn);
+        divCard.appendChild(divPhoto);
+        divCard.appendChild(divDescription);
+
+        document.body.appendChild(divCard);
+    });
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function exit() {
+    deleteCookie(authCookieName);
+    window.location.href = 'auth.html'
+}
+
+function deleteCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
